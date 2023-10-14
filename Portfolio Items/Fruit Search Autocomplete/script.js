39438de250fdc28f2f6d73fef6679366044a7191
T <noreply@theducksays.click>
2023-10-14 15:45:11 -0700

    got starter code. Added more fruit.

---
+++ Portfolio Items/Fruit Search Autocomplete/script.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+function search(str) {
+	let results = [];
+
+	// TODO
+
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+}
+
+function showSuggestions(results, inputVal) {
+
+	// TODO
+}
+
+function useSuggestion(e) {
+	// TODO
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
