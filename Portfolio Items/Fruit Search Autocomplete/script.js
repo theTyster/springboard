0ef7ba084a7e0b9f2c7eaa34d5d280235883b60a
T <noreply@theducksays.click>
2023-10-17 10:41:01 -0700

    completed fruit search project.

--- Portfolio Items/Fruit Search Autocomplete/script.js
+++ Portfolio Items/Fruit Search Autocomplete/script.js
@@ -1,61 +1,74 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-const watermelon = document.querySelector(".watermelon-img");
-const orange  = document.querySelector(".orange-img");
+const fruitContainer = Array.from(document.querySelectorAll(".fruit-sub-container"));
+const watermelon = document.querySelector(".watermelon-sub-container");
+const orange = document.querySelector(".orange-sub-container");
+const dontUnclick = ["fruit-search flex-items", "orange-img flex-items", "watermelon-img flex-items", "suggestion", "watermelon-link-text flex-items", "orange-link-text flex-items"];
 
-const clickInput = event =>{
-	orange.style.opacity = 1;
-	watermelon.style.opacity = 1;
+
+//STYLING
+const clickInput = ()=>{
+	fruitContainer.map(e=> e.style.opacity = 1);
 	input.focus();
 	if(window.visualViewport.width < 600)
-		input.style.top = "-180px";
+		input.style.top = "-270px";
 	else
 		input.style.top = "-400px";
 
 	input.style.minWidth = "200px";
 	document.addEventListener("click", unclickAnywhere, {once:true});
 }
-
-//document.addEventListener("click", (e)=> console.log(e));
-
-const dontUnclick = ["fruit-search flex-items", "orange-img flex-items", "watermelon-img flex-items", "suggestions"]
-
 const unclickAnywhere = event =>{
 	if(dontUnclick.every(elem=> event.target.className !== elem)){
-		orange.style.opacity = 0;
-		watermelon.style.opacity = 0;
+		fruitContainer.map(e=>e.style.opacity = 0);
 		input.blur();
 		input.style.minWidth = "0";
 		input.style.top = "0";
 		input.addEventListener("click", clickInput, {once:true});
+		Array.from(suggestions.children).map(e=>e.remove()); //remove all previous suggestions.
 	}
 	else {
 		document.addEventListener("click", unclickAnywhere, {once:true});
 	}
-	console.log(event);
 }
 
-function search(str) {
-	let results = [];
 
-	// TODO
+//SEARCH FUNCTIONALITY
+const search = str => fruit.filter(elem => elem.toLowerCase().includes(str.toLowerCase()));
+const autoCompleteHandler = (event) => {
+	//STYLING
+	input.removeEventListener("click", clickInput);
+	clickInput();
 
-	return results;
+	Array.from(suggestions.children).map(e=>e.remove()); //remove all previous suggestions.
+	const str = event.target.value;
+	let autoCompleted = search(str);
+
+	autoCompleted = autoCompleted.filter(elem=> (elem.toLowerCase() !== str.toLowerCase())); // remove exact matches
+	str||(autoCompleted = []); //clear the list if input is empty.
+
+	//CREATE SUGGESTIONS
+	autoCompleted.map(elem=>{
+		const li = document.createElement("li");
+		li.innerText = elem;
+		li.classList.add("suggestion");
+		suggestions.append(li);
+	})
 }
 
-function searchHandler(e) {
-
+//FILL INPUT WITH SELECTED SUGGESTION
+const useSuggestion = event=> {
+	input.value = event.target.innerText;
+	input.focus();
 }
 
-function showSuggestions(results, inputVal) {
-
-	// TODO
-}
-
-function useSuggestion(e) {
-	// TODO
+//DIRECT TO WIKIPEDIA OR OPEN FOOD FACTS
+const querySearch = event=> {
+	event.target.className.match("watermelon")? window.open(`https://en.wikipedia.org/w/index.php?search=${input.value}`, "_blank"):window.open(`https://world.openfoodfacts.org/cgi/search.pl?search_terms=${input.value}`, "_blank");
 }
 
 input.addEventListener("click", clickInput, {once:true});
-input.addEventListener('keyup', searchHandler);
+input.addEventListener('keyup', autoCompleteHandler);
 suggestions.addEventListener('click', useSuggestion);
+watermelon.addEventListener("click", querySearch);
+orange.addEventListener("click", querySearch);
