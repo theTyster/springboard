1e1cf258370094eccb9aa38a19da8e3fcf98e207
T <noreply@theducksays.click>
2023-10-20 13:31:43 -0700

    add error handling to axios.

--- Portfolio Items/Giphy Search/app.js
+++ Portfolio Items/Giphy Search/app.js
@@ -2,11 +2,20 @@
 $("form").on("submit", async function (e) {
   e.preventDefault();
   const query = $("input").val();
-  let request = await axios.get(
-    `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`,
-  );
+  let request = await axios
+    .get(
+      `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`,
+    )
+    .catch(async () => {
+      $(
+        "<p class='error'> There was an error. Please, try again. </p>",
+      ).appendTo("h1");
+      await new Promise((r) => setTimeout(() => r(), 4000)); // custom sleep function
+      $("p.error").remove();
+    });
+  console.log(request);
   request = request.data.data;
-  const gifs = request.map((e) =>
+  request.map((e) =>
     $("div.container.gifs").append(
       `<img class="gif" alt="${e.title}"src=${e.images.fixed_height.url} type="gif">`,
     ),
