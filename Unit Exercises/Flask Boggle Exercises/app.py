467d71348cb1b2dfb1a4e3a447897a9c51b3a3c0
T <noreply@theducksays.click>
2023-11-15 18:32:26 -0800

    create HTML templates and routes for boggle game.

--- Unit Exercises/Flask Boggle Exercises/app.py
+++ Unit Exercises/Flask Boggle Exercises/app.py
@@ -1,3 +1,45 @@
+#! /usr/bin/env python3
+from flask import (
+    Flask,
+    render_template,
+    request,
+    jsonify,
+    #abort,
+    #redirect,
+    #flash,
+    #make_response,
+    session,
+    url_for,
+)
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '🤫'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/', methods =['GET'])
+def start():
+
+    session['board'] = boggle_game.make_board()
+    return render_template('start.html')
+
+@app.route('/', methods = ['POST'])
+def check_for_match():
+
+    guess = request.form['guess']
+    board = session['board']
+
+    is_match = boggle_game.check_valid_word(board, guess)
+
+    result = {
+        'result': str(is_match)
+    }
+
+    result = jsonify(result)
+
+    return result
