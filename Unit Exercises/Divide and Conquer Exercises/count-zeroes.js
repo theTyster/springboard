73f328ad7e6426c8c58097306de09ad57ce60069
T <noreply@theducksays.click>
2023-12-05 18:24:53 -0800

    completes count-zeroes.js creates a template file. Starts sorted_frequency.js

--- Unit Exercises/Divide and Conquer Exercises/count-zeroes.js
+++ Unit Exercises/Divide and Conquer Exercises/count-zeroes.js
@@ -1,5 +1,29 @@
-function countZeroes() {
-  
+const countZeroes = arr => {
+  let lastIdx = arr.length - 1;
+  let firstIdx = 0;
+  let zeroCount;
+
+  // SEARCH FOR THE POINT OF DIVERGENCE BETWEEN 0'S AND 1'S 
+  while(zeroCount===undefined){
+  let middleIdx = Math.floor((lastIdx + firstIdx)/2);
+
+    //ALL VALUES ARE EQUAL
+    if(arr[firstIdx] === arr[lastIdx]){
+      if (arr[middleIdx] === 0) zeroCount = arr.length;
+      if (arr[middleIdx] === 1) zeroCount = 0;
+    }
+
+    // FOUND THE POINT OF DIVERGENCE
+    if(arr[middleIdx] + arr[middleIdx +1] === 1){
+      zeroCount = (arr.length - middleIdx)-1;
+    }
+    // MIDDLE VALUE IS ADJACENT TO A 1 MOVE firstIdx UP THE ARRAY
+    else if(arr[middleIdx] + arr[middleIdx +1] === 2){firstIdx = middleIdx}
+    // MIDDLE VALUE IS ADJACENT TO A 0 MOVE lastIdx DOWN THE ARRAY
+    else if(arr[middleIdx] + arr[middleIdx +1] === 0)lastIdx = middleIdx
+  }
+  return zeroCount;
+
 }
 
 module.exports = countZeroes
--- Unit Exercises/Divide and Conquer Exercises/sorted-frequency.js
+++ Unit Exercises/Divide and Conquer Exercises/sorted-frequency.js
@@ -1,5 +1,25 @@
-function sortedFrequency() {
+const sortedFrequency = (arr, num)=>{
+  let lastIdx = arr.length - 1;
+  let firstIdx = 0;
+  let numCount;
 
+  while(!numCount){
+  let middleIdx = Math.floor((lastIdx + firstIdx)/2);
+
+    // num is not in the array
+    if(arr[lastIdx]<num) return (-1);
+
+    // middleIdx is to high move lastIdx down
+    if(arr[middleIdx]>num) lastIdx = middleIdx-1;
+    // middleIdx is to low move firstIdx up 
+    if(arr[middleIdx]<num) firstIdx = middleIdx+1
+
+    if(arr[middleIdx]===num){
+      if(arr[middleIdx]-1 === num) firstIdx = middleIdx-1
+
+    }
+
+  }
 }
 
 module.exports = sortedFrequency
