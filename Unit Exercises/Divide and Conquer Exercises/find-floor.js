ac2340338e9f09935166447b938465f8bee8b6b5
T <noreply@theducksays.click>
2023-12-19 16:39:16 -0800

    completes the assignment. finally.

--- Unit Exercises/Divide and Conquer Exercises/find-floor.js
+++ Unit Exercises/Divide and Conquer Exercises/find-floor.js
@@ -1,17 +1,43 @@
 const findFloor=(arr, num)=> {
+
+  //This function reduces an array to three items that I have named logarithmic
+  //pointers. These three items are used to determine whether the array
+  //needs to be halfed again to find floor number in the array.
+
+  // Logarithmic pointers.
   let firstIdx = 0;
-  let lastIdx = arr.length;
-  let middleIdx = Math.floor((lastIdx-firstIdx)/2);
+  let lastIdx = arr.length -1;
+  let middleIdx = Math.floor((lastIdx + firstIdx)/2);
+
+  let sample = [arr[firstIdx], arr[middleIdx], arr[lastIdx]]
+  let highest = Math.max(...sample);
+  let lowest = Math.min(...sample);
+
+  // Nothing lower than num
+  if(num < lowest) return -1;
+  // num is highest
+  if(num > highest) return highest;
+
+  // The thing we are looking for.
   let floor;
 
-  const sample = [arr[firstIdx], arr[middleIdx], arr[lastIdx], ]
-
   while (floor===undefined){
+    middleIdx = Math.floor((lastIdx+firstIdx)/2);
+    sample = [arr[firstIdx], arr[middleIdx], arr[lastIdx]]
+    highest = Math.max(...sample);
+    lowest = Math.min(...sample);
 
+    // Exact match case
+    for (let sampleItem of sample)
+      if (sampleItem === num) floor = sampleItem;
+
+    // Array has been reduced to it's smallest form. lowest value is returned.
+    if (middleIdx === firstIdx && highest > num) floor = lowest;
+
+    if (arr[middleIdx] > num) lastIdx = middleIdx;
+    if (arr[middleIdx] < num) firstIdx = middleIdx;
   }
+  return floor
 }
 
-console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 5)); // 2
-//console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 20)); // 19
-//console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 0)); // -1
 module.exports = findFloor
