ef42d742e4b86a31ec36bf1ddde62910ecfa7821
T <noreply@mail.thetyster.dev>
2024-01-03 13:36:41 -0800

    adds a javascript file for inserting the correct currency symbol

---
+++ src/static/main.js
@@ -0,0 +1,56 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main(){
+
+  const convertFrom = document.querySelector('#convert-from').value;
+  const convertTo = document.querySelector('#convert-to').value;
+  const convertAmount = Number(document.querySelector('#currency-amount').value);
+  const conversionResult = Number(document.querySelector('#conversion-result').innerText);
+  
+  const dcFrom = new DisplayCurrency(convertAmount, convertFrom);
+  const dcTo = new DisplayCurrency(conversionResult, convertTo);
+
+  const resultElem = document.querySelector('#currency-converted');
+	
+  
+  if(resultElem) resultElem.innerHTML = `<p>${dcFrom.withSymbol()} is equal to <span style="color:green;">${dcTo.withSymbol()}</span>.</p>`
+}
+
+class DisplayCurrency{
+  constructor(num, currency){
+    this.num = num;
+    this.currency = currency;
+    this.locale = this.getLanguage();
+  }
+
+  // guesses the local language from the browser.
+  getLanguage(){
+    if (navigator.languages && navigator.languages.length) {
+      return navigator.languages[0];
+    } else {
+      return navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en';
+    }
+  }
+
+  withSymbol(){
+    return new Intl
+      .NumberFormat(this.locale, 
+        { style: 'currency', currency: this.currency })
+      .formatToParts(this.num)
+      .map(val => val.value)
+      .join('');
+  }
+
+  withoutSymbol(){
+    return new Intl
+      .numberformat(this.locale, 
+      { style: 'currency', currency: this.currency })
+      .formattoparts(this.num)
+      .slice(1)
+      .map(val => val.value)
+      .join('');
+   }
+}
+
