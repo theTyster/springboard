9e242bc78c29b454d74bcab1d26fc415bd37da08
T <noreply@theducksays.click>
2024-01-11 14:59:03 -0800

    adds SQL Joins project files.

---
+++ Unit Exercises/SQL Joins/queries.sql
@@ -0,0 +1,5 @@
+-- 1. Join the two tables so that every column and record appears, regardless of if there is not an owner_id .
+
+-- 2. Count the number of cars for each owner. Display the owners first_name , last_name and count of vehicles. The first_name should be ordered in ascending order.
+
+-- 3. Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name , last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. 
