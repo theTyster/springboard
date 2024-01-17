da2349c6725143792c83650d41095bf0bcf368b6
T <noreply@theducksays.click>
2024-01-16 17:20:58 -0800

    adds skeleton for blogly app.

--- Unit Exercises/SQLAlchemy Exercises/app.py
+++ Unit Exercises/SQLAlchemy Exercises/app.py
@@ -1,12 +1,27 @@
+# The requirements.txt file that Colt uses is outdated.
+# This app was built using modern tools.
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Users
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = '🤫'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
-connect_db(app)
-db.create_all()
+with app.app_context():
+    connect_db(app)
+    db.create_all()
+
+@app.route('/')
+def check():
+    '''
+    Checks that flask is running.
+    '''
+    return 'Flask is working'
