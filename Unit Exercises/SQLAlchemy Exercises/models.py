c45ce47307d75bf1c1f756f90ff01c9afcf6e19f
T <noreply@theducksays.click>
2024-01-19 18:22:00 -0800

    Adds Posts model.

--- Unit Exercises/SQLAlchemy Exercises/models.py
+++ Unit Exercises/SQLAlchemy Exercises/models.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 """Models for Blogly."""
 from utils import SQLAlchemyUtils
+from datetime import datetime
 
 sql = SQLAlchemyUtils()
 
@@ -21,3 +22,35 @@ class Users(sql.db.Model):
                      nullable=False)
 
     image_url = sql.db.Column(sql.db.String())
+
+    posts = sql.db.relationship(
+        'Posts',
+        cascade='all, delete-orphan',
+        lazy=True
+    )
+
+
+class Posts(sql.db.Model):
+    '''
+    User Posts postgres model.
+    '''
+    __tablename__ = 'posts'
+
+    id = sql.db.Column(sql.db.Integer,
+                       primary_key=True,
+                       autoincrement=True)
+
+    title = sql.db.Column(sql.db.String(),
+                          nullable=False)
+
+    content = sql.db.Column(sql.db.String(),
+                            nullable=False)
+
+    created_at = sql.db.Column(sql.db.DateTime,
+                               default=datetime.utcnow)
+
+    user_id = sql.db.Column(sql.db.Integer(),
+                            sql.db.ForeignKey('users.id'),
+                            nullable=False)
+
+    user = sql.db.relationship(Users, lazy=True)
