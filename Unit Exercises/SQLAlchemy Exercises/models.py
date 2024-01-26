3c607fb88ea72415a78f3a5e7c018c4deb052036
T <noreply@theducksays.click>
2024-01-25 16:41:28 -0800

    adds tags model and post_tag model using many-to-many database structure.

--- Unit Exercises/SQLAlchemy Exercises/models.py
+++ Unit Exercises/SQLAlchemy Exercises/models.py
@@ -15,16 +15,17 @@ class Users(sql.db.Model):
                    primary_key=True,
                    autoincrement=True)
 
-    first_name = sql.db.Column(sql.db.String(),
+    first_name = sql.db.Column(sql.db.String,
                      nullable=False)
 
-    last_name = sql.db.Column(sql.db.String(),
+    last_name = sql.db.Column(sql.db.String,
                      nullable=False)
 
-    image_url = sql.db.Column(sql.db.String())
+    image_url = sql.db.Column(sql.db.String)
 
     posts = sql.db.relationship(
         'Posts',
+        backref='user',
         cascade='all, delete-orphan',
         lazy=True
     )
@@ -38,19 +39,49 @@ class Posts(sql.db.Model):
 
     id = sql.db.Column(sql.db.Integer,
                        primary_key=True,
-                       autoincrement=True)
+                       autoincrement=True,
+                       )
 
-    title = sql.db.Column(sql.db.String(),
+    title = sql.db.Column(sql.db.String,
                           nullable=False)
 
-    content = sql.db.Column(sql.db.String(),
+    content = sql.db.Column(sql.db.String,
                             nullable=False)
 
     created_at = sql.db.Column(sql.db.DateTime,
-                               default=datetime.utcnow)
+                               default=datetime.utcnow,
+                               nullable=False)
 
-    user_id = sql.db.Column(sql.db.Integer(),
+    user_id = sql.db.Column(sql.db.Integer,
                             sql.db.ForeignKey('users.id'),
                             nullable=False)
 
-    user = sql.db.relationship(Users, lazy=True)
+
+class Tags(sql.db.Model):
+    '''
+    Tags postgres model.
+    '''
+    id = sql.db.Column(sql.db.Integer,
+                       primary_key=True,
+                       autoincrement=True)
+
+    name = sql.db.Column(sql.db.String,
+                         unique=True,
+                         nullable=False)
+
+    posts = sql.db.relationship('Posts',
+                                secondary='post_tag',
+                                cascade="all, delete",
+                                backref='tags',
+                                lazy=True)
+
+
+class PostTag(sql.db.Model):
+    __tablename__ = 'post_tag'
+    post_id = sql.db.Column(sql.db.Integer,
+                            sql.db.ForeignKey('posts.id'),
+                            primary_key=True)
+
+    tag_id = sql.db.Column(sql.db.Integer,
+                           sql.db.ForeignKey('tags.id'),
+                           primary_key=True)
